c2b257b73b607a699798354e8ce50646825f3ab7
AmarKoonar <amarkoonar@gmail.com>
2025-07-14 15:31:52 -0700

    create working query to github API and update gitignore

--- src/app/api/github_api/issue_finder/route.js
+++ src/app/api/github_api/issue_finder/route.js
@@ -1,4 +1,21 @@
 import { NextResponse } from "next/server";
+const token = process.env.GITHUB_TOKEN;
 export async function GET(req) {
-  return NextResponse.json("test");
+    const { searchParams } = new URL(req.url);
+    const language = searchParams.get("lang") || "JavaScript";
+
+    const query = `language:${language} label:"good first issue" label:"help wanted" state:open`;
+    const url = `https://api.github.com/search/issues?q=${encodeURIComponent(
+        query
+    )}&per_page=50&sort=created&order=desc`;
+
+    const authHeaders = {
+        Authorization: `Bearer ${token}`,
+    };
+
+    const res = await fetch(url, { headers: authHeaders });
+    const data = await res.json();
+    console.log(data);
+
+    return NextResponse.json(data);
 }
