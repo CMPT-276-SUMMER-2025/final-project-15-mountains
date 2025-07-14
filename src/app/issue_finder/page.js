a96cbab938077f2fdf6eeeb2715f9eea83499cdb
AmarKoonar <amarkoonar@gmail.com>
2025-07-14 16:01:43 -0700

    remove testing styles

--- src/app/issue_finder/page.js
+++ src/app/issue_finder/page.js
@@ -28,13 +28,13 @@ export default function IssueSelector() {
     };
     return (
         <div>
-        <div className="flex flex-col justify-end items-center h-[20vh] border-">
+        <div className="flex flex-col justify-end items-center h-[20vh] mb-15">
             <h1 className="text-7xl">Issue Finder</h1>
         </div>
-        <div className="flex flex-col justify-center items-center mt-5 border-">
-        <Card className="p-6 flex flex-row justify-center px-10 mb-3 border-2">
+        <div className="flex flex-col justify-center items-center mt-5">
+        <Card className="p-6 flex flex-row justify-center px-10 mb-3">
             
-            <div className="space-y-4 border-r-5 pr-15 flex flex-col justify-center items-center">
+            <div className="space-y-4 border-r-5 pr-10 flex flex-col justify-center items-center">
                 <h2 className="text-xl font-semibold">
                     Find Good First Issues
                 </h2>
@@ -75,7 +75,7 @@ export default function IssueSelector() {
                 </Button>
             </div>
             </Card>
-            <div className="border-0 bg-transparent w-200 ">
+            <div className="border-0 bg-transparent w-200 mt-5">
                 {issues.map((issue, i) => (
                 <Card key={issue.id} id={`issue-${i}`} className={"p-4 transition-all duration-300 mb-5"}>
                     <a href={issue.html_url} className="underline font-medium text-lg">{issue.title}</a>
