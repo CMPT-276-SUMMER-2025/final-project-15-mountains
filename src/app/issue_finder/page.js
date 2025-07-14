5aea0fbd6f8a78b7dd58b66fce02da5f209d9030
AmarKoonar <amarkoonar@gmail.com>
2025-07-14 15:59:38 -0700

    add issues ui

--- src/app/issue_finder/page.js
+++ src/app/issue_finder/page.js
@@ -17,12 +17,13 @@ export default function IssueSelector() {
     const languages = ["JavaScript", "Python", "TypeScript", "Go", "Java", "C++", "Rust"];
 
     const [loading,setLoading] = useState(false);
+    const [issues,setIssues] = useState([]);
 
     const fetchIssues = async () => {
         setLoading(true);
-        const response = await fetch("/api/github_api/issue_finder");
-        const data = await response.json();
-        console.log(data);
+        const res = await fetch(`/api/github_api/issue_finder?lang=${selectedLang}`);
+        const data = await res.json();
+        setIssues(data);
         setLoading(false);
     };
     return (
@@ -35,10 +36,10 @@ export default function IssueSelector() {
             
             <div className="space-y-4 border-r-5 pr-15 flex flex-col justify-center items-center">
                 <h2 className="text-xl font-semibold">
-                Find Good First Issues
+                    Find Good First Issues
                 </h2>
                 <p className="text-sm font-semibold">
-                Select a language and search for GitHub issues.
+                    Select a language and search for GitHub issues.
                 </p>
 
                 <Select value={selectedLang} onValueChange={setSelectedLang}>
@@ -68,16 +69,26 @@ export default function IssueSelector() {
                 Can't pick? Let AI decide.
                 </p>
 
-                <Input
-                placeholder="Tell us what kind of issue you want..."
-                disabled={true}
-                className="w-64"
-                />
-                <Button className="hover:cursor-pointer">
-                Ask AI to Choose
+                <Input placeholder="Tell us what kind of issue you want..." disabled={true} className="w-64" />
+                <Button disabled={true} className="hover:cursor-pointer">
+                    Ask AI to Choose
                 </Button>
             </div>
             </Card>
+            <div className="border-0 bg-transparent w-200 ">
+                {issues.map((issue, i) => (
+                <Card key={issue.id} id={`issue-${i}`} className={"p-4 transition-all duration-300 mb-5"}>
+                    <a href={issue.html_url} className="underline font-medium text-lg">{issue.title}</a>
+                    <div className="text-sm text-gray-200 mt-2">
+                        <p><strong>Repo:</strong> <a href={issue.repository.html_url} target="_blank" className="underline">{issue.repository.name}</a></p>
+                        <p><strong>Repo Description:</strong> {issue.repository.description || "No description"}</p>
+                        <p><strong>Stars:</strong> {issue.repository.stargazers_count} &nbsp;&nbsp; <strong>🍴 Forks:</strong> {issue.repository.forks_count}</p>
+                    </div>
+                </Card>
+            ))}
+                </div>
+
+            
 
         </div>
 
