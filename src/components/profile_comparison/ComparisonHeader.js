8f2a1eaccca7c7967942509bffe4e71ad555a7cb
denoire <thomaszac19@gmail.com>
2025-07-14 23:25:04 -0700

    Add basic ComparisonHeader component with placeholder title and subtitle for the profile comparison page

---
+++ src/components/profile_comparison/ComparisonHeader.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function ComparisonHeader() {
+    return (
+        <div className="border border-red-500 text-center mb-15">
+            <h1 className="text-5xl font-extrabold text-black">
+                Profile Comparison
+            </h1>
+            <p className="text-gray-500 text-lg mt-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing.
+            </p>
+        </div>
+    );
+}
