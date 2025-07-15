6e86414fc59d4bce885ff7e9f8c9d7ebf262b76e
denoire <thomaszac19@gmail.com>
2025-07-15 05:28:24 -0700

    Refactor ProfileComparison and UserSlot logic to allow a maximum of 4 users

--- src/app/profile_comparison/page.js
+++ src/app/profile_comparison/page.js
@@ -1,22 +1,32 @@
 "use client";
 import { useState, useRef } from "react";
 import ComparisonHeader from "@/components/profile_comparison/ComparisonHeader";
+import SearchBar from "@/components/profile_comparison/SearchBar";
 import UserSlot from "@/components/profile_comparison/UserSlot";
 
 export default function ProfileComparison() {
-    const [users, setUsers] = useState([null, null]);
+    const [users, setUsers] = useState([]);
     const cacheRef = useRef({});
 
-    const updateUser = (userObject, index) => {
-        const updated = [...users];
-        updated[index] = userObject;
-        setUsers(updated);
+    const addUser = (userObject) => {
+        if (users.length >= 4) return;
+
+        if (!users.some((u) => u.login === userObject.login)) {
+            setUsers([...users, userObject]);
+        }
     };
 
     return (
         <div className="border border-red-500 m-10 p-5 mt-25">
             <ComparisonHeader />
-            <UserSlot users={users} updateUser={updateUser} cacheRef={cacheRef} />
+            <div className="border border-red-500 flex flex-col gap-5">
+                <div className="border border-red-500 flex justify-center">
+                    <div className="border border-red-500 w-1/3">
+                        <SearchBar onSelect={addUser} cache={cacheRef}/>
+                    </div>
+                </div>
+                <UserSlot users={users}/>
+            </div>
         </div>
     );
 }
--- src/components/profile_comparison/UserSlot.js
+++ src/components/profile_comparison/UserSlot.js
@@ -1,35 +1,23 @@
 "use client";
-import SearchBar from "@/components/profile_comparison/SearchBar";
 
 export default function UserSlot({ users, updateUser, cacheRef }) {
     return (
-        <div className="border border-red-500">
-            <div className="flex flex-row justify-center gap-10">
-                {users.map((user, index) => (
-                    <div
-                        key={index}
-                        className="border border-red-500 flex flex-col items-center gap-5 p-5"
-                    >
-                        <SearchBar
-                            onSelect={(user) => updateUser(user, index)}
-                            cache={cacheRef}
-                        />
-
-                        {user && (
-                            <div className="border border-red-500 flex flex-col items-center">
-                                <img
-                                    src={user.avatar_url}
-                                    alt={user.login}
-                                    className="border-red-500 border-2 w-30 h-30 rounded-full"
-                                />
-                                <p className="border border-red-500 mt-2 font-semibold">
-                                    @{user.login}
-                                </p>
-                            </div>
-                        )}
-                    </div>
-                ))}
-            </div>
+        <div className="border border-red-500 flex flex-wrap justify-center gap-5">
+            {users.map((user, index) => (
+                <div
+                    key={index}
+                    className="border border-red-500 flex flex-col items-center gap-5 p-5"
+                >
+                    <img
+                        src={user.avatar_url}
+                        alt={user.login}
+                        className="border-red-500 border-2 w-30 h-30 rounded-full"
+                    />
+                    <p className="border border-red-500 mt-2 font-semibold">
+                        @{user.login}
+                    </p>
+                </div>
+            ))}
         </div>
     );
 }
