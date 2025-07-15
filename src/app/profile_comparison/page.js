36a39800f4f3aeaa0f26996209c1edba6fa6e0e1
denoire <thomaszac19@gmail.com>
2025-07-15 03:59:26 -0700

    Encapsulate search UI as UserSlot component

--- src/app/profile_comparison/page.js
+++ src/app/profile_comparison/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useRef } from "react";
 import ComparisonHeader from "@/components/profile_comparison/ComparisonHeader";
-import SearchBar from "@/components/profile_comparison/SearchBar";
+import UserSlot from "@/components/profile_comparison/UserSlot";
 
 export default function ProfileComparison() {
     const [users, setUsers] = useState([null, null]);
@@ -16,33 +16,7 @@ export default function ProfileComparison() {
     return (
         <div className="border border-red-500 m-10 p-5 mt-25">
             <ComparisonHeader />
-
-            <div className="border border-red-500">
-                <div className="flex flex-row justify-center gap-10">
-                    {users.map((user, index) => (
-                        <div
-                            key={index}
-                            className="border border-red-500 flex flex-col items-center gap-5 p-5"
-                        >
-                            <SearchBar
-                                onSelect={(user) => updateUser(user, index)}
-                                cache={cacheRef}
-                            />
-
-                            {user && (
-                                <div className="border border-red-500 flex flex-col items-center">
-                                    <img
-                                        src={user.avatar_url}
-                                        alt={user.login}
-                                        className="border-red-500 border-2 w-30 h-30 rounded-full"
-                                    />
-                                    <p className="border border-red-500 mt-2 font-semibold">@{user.login}</p>
-                                </div>
-                            )}
-                        </div>
-                    ))}
-                </div>
-            </div>
+            <UserSlot users={users} updateUser={updateUser} cacheRef={cacheRef} />
         </div>
     );
 }
