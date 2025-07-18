23d30b9cb977328991c3e44a49a8c077ba13c050
denoire <thomaszac19@gmail.com>
2025-07-18 16:52:37 -0700

    Move the Analyze button to ProfileComparison and add template analysis view, auto-hide when no users are queued

--- src/app/profile_comparison/page.js
+++ src/app/profile_comparison/page.js
@@ -1,11 +1,14 @@
 "use client";
-import { useState, useRef } from "react";
+import {useState, useRef, useEffect} from "react";
 import ComparisonHeader from "@/components/profile_comparison/ComparisonHeader";
 import SearchBar from "@/components/profile_comparison/SearchBar";
 import UserSlot from "@/components/profile_comparison/UserSlot";
+import {FaArrowRight} from "react-icons/fa";
 
 export default function ProfileComparison() {
     const [users, setUsers] = useState([]);
+    const [showAnalysis, setShowAnalysis] = useState(false);
+
     const cacheRef = useRef({});
 
     const addUser = (userObject) => {
@@ -20,17 +23,45 @@ export default function ProfileComparison() {
         setUsers(users.filter((u) => u.login !== login));
     };
 
+    const handleAnalysis = () => {
+        if (users.length > 0) setShowAnalysis(true);
+        console.log(users);
+    }
+
+    useEffect(() => {
+        if (users.length === 0) setShowAnalysis(false);
+    }, [users]);
+
     return (
         <div className="border border-red-500 m-10 p-5 mt-25">
             <ComparisonHeader />
             <div className="border border-red-500 flex flex-col gap-5">
                 <div className="border border-red-500 flex justify-center">
-                    <div className="w-1/3">
-                        <SearchBar onSelect={addUser} cache={cacheRef}/>
+                    <div className="flex flex-row justify-center w-1/3 gap-5">
+                        <SearchBar onSelect={addUser} onRemove={removeUser} cache={cacheRef}/>
+                        <button
+                            type="submit"
+                            onClick={handleAnalysis}
+                            className={`flex items-center gap-2 px-6 py-3 rounded-lg text-white font-bold transition 
+                            bg-red-500 hover:bg-red-600 active:bg-red-700`}
+                        >
+                            <FaArrowRight className="text-white"/>
+                            ANALYZE
+                        </button>
                     </div>
                 </div>
-                <UserSlot users={users} removeUser={removeUser} />
+
+                <UserSlot users={users} removeUser={removeUser}/>
             </div>
+
+            {showAnalysis && (
+                <div className="mt-5">
+                    <div className="flex h-screen bg-gray-50 rounded-lg border border-2 border-grey-400">
+                        <div className="">
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/profile_comparison/SearchBar.js
+++ src/components/profile_comparison/SearchBar.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { Debounce } from "@/components/profile_comparison/Debounce";
 import { SearchCache } from "@/components/profile_comparison/SearchCache";
-import { FaGithub, FaArrowRight } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 export default function SearchBar({ onSelect, cache }) {
     const [input, setInput] = useState("");
@@ -22,75 +22,54 @@ export default function SearchBar({ onSelect, cache }) {
         setResultsVisible(false);
     };
 
-    const handleAnalyze = (e) => {
-        e.preventDefault();
-        if (results.length > 0) handleSelect(results[0]);
-    }
-
     return (
-        <form
-            onSubmit={handleAnalyze}
-            className="flex items-center w-full gap-4"
-        >
-            <div
-                className="relative flex items-center border rounded px-4 py-3 bg-white">
-                <FaGithub className="text-gray-700 text-xl mr-3 h-6 w-6"/>
-                <input
-                    type="text"
-                    placeholder="Search GitHub users..."
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    onFocus={() => {
-                        if (results.length > 0) setResultsVisible(true);
-                    }}
-                    onBlur={() => {
-                        setTimeout(() => setResultsVisible(false), 100);
-                    }}
-                    className="outline-none text-gray-800"
-                />
+        <div className="relative flex items-center border rounded px-4 py-3 bg-white">
+            <FaGithub className="text-gray-700 text-xl mr-3 h-6 w-6"/>
+            <input
+                type="text"
+                placeholder="Search GitHub users..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onFocus={() => {
+                    if (results.length > 0) setResultsVisible(true);
+                }}
+                onBlur={() => {
+                    setTimeout(() => setResultsVisible(false), 100);
+                }}
+                className="outline-none text-gray-800"
+            />
 
-                {input && (
-                    <button
-                        type="button"
-                        onClick={() => setInput("")}
-                        className="absolute flex items-center justify-center rounded right-3 w-6 h-6
-                                   hover:cursor-pointer hover:bg-gray-100 transition"
-                    >
-                        <span className="text-lg text-gray-800">&times;</span>
-                    </button>
-                )}
+            {input && (
+                <button
+                    type="button"
+                    onClick={() => setInput("")}
+                    className="absolute flex items-center justify-center rounded right-3 w-6 h-6
+                               hover:cursor-pointer hover:bg-gray-100 transition"
+                >
+                    <span className="text-lg text-gray-800">&times;</span>
+                </button>
+            )}
 
-                {resultsVisible && results.length > 0 && (
-                    <ul className="absolute top-full left-0 mt-1 bg-white w-full border rounded shadow z-1">
-                        {results.map((user) => (
-                            <li
-                                key={user.id}
-                                onMouseDown={() => handleSelect(user)}
-                                className="cursor-pointer rounded px-3 py-2 hover:bg-gray-100"
-                            >
-                                <div className="flex items-center gap-3">
-                                    <img
-                                        src={user.avatar_url}
-                                        alt={user.login}
-                                        className="w-6 h-6 rounded-full"
-                                    />
-                                    <span className="truncate">{user.login}</span>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-
-            <button
-                type="submit"
-                className={`flex items-center gap-2 px-6 py-3 rounded-lg text-white font-bold transition 
-                            bg-red-500 hover:bg-red-600 active:bg-red-700`}
-            >
-                <FaArrowRight className="text-white"/>
-                ANALYZE
-            </button>
-        </form>
-
+            {resultsVisible && results.length > 0 && (
+                <ul className="absolute top-full left-0 mt-1 bg-white w-full border rounded shadow z-1">
+                    {results.map((user) => (
+                        <li
+                            key={user.id}
+                            onMouseDown={() => handleSelect(user)}
+                            className="cursor-pointer rounded px-3 py-2 hover:bg-gray-100"
+                        >
+                            <div className="flex items-center gap-3">
+                                <img
+                                    src={user.avatar_url}
+                                    alt={user.login}
+                                    className="w-6 h-6 rounded-full"
+                                />
+                                <span className="truncate">{user.login}</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
 }
