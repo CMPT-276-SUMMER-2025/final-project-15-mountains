62b93e6ce18d2c4bfdb8edf9f2028a3e51209491
AmarKoonar <amarkoonar@gmail.com>
2025-07-24 19:30:21 -0700

    Improve Readme_viewer UI and increase amount of info returned from issue_finder route

--- src/app/readme_viewer/page.js
+++ src/app/readme_viewer/page.js
@@ -9,13 +9,22 @@ import ReactMarkdown from "react-markdown"
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import { Textarea } from "@/components/ui/textarea";
-
+import {ScreenFullIcon} from '@primer/octicons-react';
+import {ScreenNormalIcon} from '@primer/octicons-react';
 export default function Page() {
     const [markdown,setMarkdown] = useState();
     const [Username,setUsername] = useState();
     const [Repo,setRepo] = useState();
     const [decodedContent,setDecodedContent] = useState();
     const [errorMessage,setErrorMessage] = useState("");
+    
+    
+    const [sizeup, setsizeup] = useState(false);
+
+    const togglesize = () =>{
+        setsizeup(!sizeup);
+
+    }
 
     //Update Text for the README.MD
     const updateText = (event) => {
@@ -71,16 +80,16 @@ export default function Page() {
     }
 
     return (
-        <div className="w-full h-auto min-h-[100vh]">
+        <div className="w-full h-auto min-h-[100vh] flex flex-col">
             {/* Header */}
             <div className="flex flex-col mx-auto pt-30 text-center">
-                <h1 className="text-4xl text-center">README Viewer</h1>
-                <p className="text-center">Enter your GitHub username and the repository, to view your README markdown file.</p>
+                <h1 className="text-7xl text-center">README Viewer</h1>
+                <p className="text-center text-md">Enter your GitHub username and the repository, to view your README markdown file.</p>
             
                 {/* Input Section */}
                 <div className="flex grid-cols-3 gap-4 justify-center py-4">
-                    <Input className="border-black border-1 rounded-full w-50" id="Username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}></Input>
-                    <Input className="border-black border-1 rounded-full w-50" id="Repository" placeholder="Repository" onChange={(e) => setRepo(e.target.value)}></Input>
+                    <Input className="border-black dark:border-gray-500 border-1  w-50" id="Username" placeholder="Username" onChange={(e) => setUsername(e.target.value)}></Input>
+                    <Input className="border-black dark:border-gray-500 border-1 w-50" id="Repository" placeholder="Repository" onChange={(e) => setRepo(e.target.value)}></Input>
                     <Button className="hover:cursor-pointer" onClick={fetchMarkdown}>Load</Button>
                 </div>
             </div>
@@ -90,10 +99,15 @@ export default function Page() {
                 {errorMessage}
             </div>
 
-            <Card className="flex flex-row gap-4 p-6 min-h-[600px]">
+            <Card className={`relative flex flex-row gap-4 p-6 min-h-[600px]  ${sizeup ? "w-full" : "w-4/6"} self-center transition-all duration-300`}>
+                <button onClick={togglesize} className="absolute right-2 top-2 z-50 cursor-pointer">
+                    {!sizeup && <ScreenFullIcon size={16} />}
+                    {sizeup && <ScreenNormalIcon size={16} />}
+                </button>
 
                 {/* The Left side of the Card */}
-                <div className="w-1/2 flex flex-col">
+                <div className="w-1/2 flex flex-col rounded-2xl border-2 dark:border-gray-500">
+                    <div className="self-center border-b-2 w-full text-center py-5 text-3xl dark:border-gray-500">Rendered Markdown</div>
                     <div className="flex-1 h-full p-4 rounded-xl overflow-auto">
                     <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={remarkGfm}>{markdown}</ReactMarkdown>
                     </div>
@@ -101,8 +115,9 @@ export default function Page() {
 
 
                 {/* The Right side of the card */}
-                <div className="w-1/2 flex flex-col">
-                    <Textarea className="flex-1 resize-none h-full rounded-xl" value={decodedContent} onChange={updateText} placeholder="Start typing to see a preview of your Markdown file">{decodedContent}</Textarea>
+                <div className="w-1/2 flex flex-col rounded-2xl border-2 dark:border-gray-500">
+                    <div className="self-center border-b-2 w-full text-center py-5  text-3xl dark:border-gray-500">Markdown Editor</div>
+                    <Textarea className="flex-1 resize-none h-full rounded-xl rounded-t-none border-t-0 text-white" value={decodedContent} onChange={updateText} placeholder="Start typing to see a preview of your Markdown file">{decodedContent}</Textarea>
                 </div>
                 
             </Card>
