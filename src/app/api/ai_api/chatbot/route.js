e2809c9eb982171b9624b5b350e1163002e398e4
euidml <ela145sfu.ca>
2025-07-29 01:47:33 -0700

    prompted the instruction to the model

--- src/app/api/ai_api/chatbot/route.js
+++ src/app/api/ai_api/chatbot/route.js
@@ -13,6 +13,14 @@ export async function POST(req) {
     const { prompt, messages } = await req.json();
 
     const systemPrompt = `
+      You are an assistant that helps developers find the most suitable GitHub issue to work on.
+      Based on the previous messages and the user prompt, generate the BEST answer fitting to resolve the question(s) in the prompt. make sure the reasoning is valid and not just fluff. 
+      Also make sure to finish your sentences
+      
+
+      Return:
+      - A reponsible answer to the user's prompt
+      - You can touch the answer with markdown if it is valid or requested .
 
         previous messages:
         ${messages.map((message) => `${message.role}: ${message.content}`).join("\n")}
@@ -21,8 +29,8 @@ export async function POST(req) {
         ${prompt}
     `;
 
-    // await console.log(systemPrompt);
       const res = await client.chat.completions.create({
+        // TODO: change to o4-mini
           model: "openai/gpt-4.1-mini",
           temperature: 0.7,
           max_tokens: 300,
