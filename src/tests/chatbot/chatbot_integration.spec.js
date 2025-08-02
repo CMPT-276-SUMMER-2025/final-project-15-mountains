c4e65fdabeedd0404c933828a92ab92ee323140c
euidml <ela145sfu.ca>
2025-08-01 19:20:21 -0700

    changed the test method for integration test

--- src/tests/chatbot/chatbot_integration.spec.js
+++ src/tests/chatbot/chatbot_integration.spec.js
@@ -1,19 +1,16 @@
 const { test, expect } = require('@playwright/test');
 
 test('INTEGRATION TEST AI CHATBOT: loads AI explanation after entering prompt', async ({ page }) => {
-    // test.setTimeout(1000_000);
+  
+  await page.goto('/chatbot');
+  
+  const aiInput = await page.getByTestId('chatbot-input');
+  await aiInput.fill('what is the best way to contribute to this project?');
 
-    await page.goto('/chatbot');
+  test.setTimeout(3_0000);
+  await page.keyboard.press('Enter');
   
-    const aiInput = await page.getByTestId('chatbot-input');
-    await aiInput.fill('what is the best way to contribute to this project?');
-  
-  
-    
-  
-    await page.getByTestId('chatbot-send-button').click();
-  
-    const response = await page.getByText('Sorry, something went wrong. Please try again.');
-    await expect(response).not.toBeVisible();
-  });
+  const response = await page.getByText('Sorry, something went wrong. Please try again.');
+  await expect(response).not.toBeVisible();
+});
   
